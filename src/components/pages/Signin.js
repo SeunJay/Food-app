f7cbc73aa524aff96c99937865367bb5fe5e0af2
seun jay <seunjay92@gmail.com>
2019-08-03 14:46:29 +0100

    implement signin method

--- src/components/pages/Signin.js
+++ src/components/pages/Signin.js
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../../components/layouts/NavBar";
+import { Redirect } from "react-router-dom";
+import { signin } from "../../auth";
 
 export default function Signin() {
-  console.log(process.env)
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    redirectToReferrer: false
+  });
+
+  const { email, password, loading, error, redirectToReferrer } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        setValues({
+          ...values,
+          redirectToReferrer: true
+        });
+      }
+    });
+  };
+
+  const showError = () => {
+    return (
+      <div
+        className="alert alert-danger"
+        style={{ display: error ? "" : "none" }}
+      >
+        {values.error}
+      </div>
+    );
+  };
+
+  const showLoading = () => {
+    return (
+      loading && (
+        <div className="alert alert-info">
+          <h2>LOADING...</h2>
+        </div>
+      )
+    );
+  };
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to="/" />;
+    }
+  };
+
   return (
     <>
       <NavBar brand="Omnifood" />
@@ -10,24 +67,29 @@ export default function Signin() {
         <div className="card mb-3">
           <div className="card-header text-center">Sign in</div>
           <div className="card-body">
-            <form>
+            {showError()}
+            {showLoading()}
+            {redirectUser()}
+            <form onSubmit={handleSubmit}>
               <div className="form-group">
                 <label className="text-warning">Email</label>
                 <input
-                  // onChange={handleChange("email")}
+                  onChange={handleChange("email")}
                   type="email"
                   className="form-control"
-                  // value={email}
+                  value={values.email}
+                  name="email"
                 />
               </div>
 
               <div className="form-group">
                 <label className="text-warning">Password</label>
                 <input
-                  // onChange={handleChange("password")}
+                  onChange={handleChange("password")}
                   type="password"
                   className="form-control"
-                  // value={password}
+                  value={values.password}
+                  name="password"
                 />
               </div>
               <button className="btn btn-warning" style={{ color: "#fff" }}>
