b19b3bcb73227bb61ab5aad1a139fa71143fb677
seun jay <seunjay92@gmail.com>
2019-08-03 11:00:52 +0100

    create onChange handler

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 //import TextField from "../layouts/TextField";
 import NavBar from "../../components/layouts/NavBar";
-import {API} from "../../config"
+import { API } from "../../config";
 
 export default function Signup() {
   console.log(process.env.REACT_APP_API_URL);
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
   return (
     <>
       <NavBar brand="Omnifood" />
-      <div className="container">
+      <div className="container col-md-8 offset-md-2">
         <div className="card mb-3">
           <div className="card-header text-center">Sign up</div>
           <div className="card-body">
@@ -16,40 +29,40 @@ export default function Signup() {
               <div className="form-group">
                 <label className="text-warning">First Name</label>
                 <input
-                  // onChange={handleChange("name")}
+                  onChange={handleChange("firstName")}
                   type="text"
                   className="form-control"
-                  // value={name}
+                  value={values.firstName}
                 />
               </div>
 
               <div className="form-group">
                 <label className="text-warning">Last Name</label>
                 <input
-                  // onChange={handleChange("name")}
+                  onChange={handleChange("lastName")}
                   type="text"
                   className="form-control"
-                  // value={name}
+                  value={values.lastName}
                 />
               </div>
 
               <div className="form-group">
                 <label className="text-warning">Email</label>
                 <input
-                  // onChange={handleChange("email")}
+                  onChange={handleChange("email")}
                   type="email"
                   className="form-control"
-                  // value={email}
+                  value={values.email}
                 />
               </div>
 
               <div className="form-group">
                 <label className="text-warning">Password</label>
                 <input
-                  // onChange={handleChange("password")}
+                  onChange={handleChange("password")}
                   type="password"
                   className="form-control"
-                  // value={password}
+                  value={values.password}
                 />
               </div>
               <button className="btn btn-warning" style={{ color: "#fff" }}>
@@ -58,7 +71,8 @@ export default function Signup() {
             </form>
           </div>
         </div>
-      {API}
+        {API}
+        {JSON.stringify(values)}
       </div>
     </>
   );
