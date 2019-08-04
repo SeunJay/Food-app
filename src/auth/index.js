fe04f81f7485251ce7f952a2c40a4da1d24b8aba
seun jay <seunjay92@gmail.com>
2019-08-04 21:33:24 +0100

    create isAuthenticated method

--- src/auth/index.js
+++ src/auth/index.js
@@ -51,3 +51,14 @@ export const signout = cb => {
       .catch(err => console.log(err));
   }
 };
+
+export const isAuthenticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
