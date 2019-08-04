63ec7a2a1371b8ed6b1e8c1f1607026824ad262e
seun jay <seunjay92@gmail.com>
2019-08-04 22:09:37 +0100

    import isAuthenticated method to conditionally render links

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
-import { signout } from "../../auth";
+import { signout, isAuthenticated } from "../../auth";
 
 function NavBar({ brand, history }) {
   return (
@@ -15,44 +15,52 @@ function NavBar({ brand, history }) {
         </Link>
         <div>
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link to="/" className="nav-link" style={{ fontWeight: "800" }}>
                 <i className="fas fa-home" /> Home
               </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/signin"
-                className="nav-link"
-                // style={isActive(history, "/signin")}
-                style={{ fontWeight: "800" }}
-              >
-                <i className="fas fa-plus" /> Sign in
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/signup"
-                className="nav-link"
-                // style={isActive(history, "/signup")}
-                style={{ fontWeight: "800" }}
-              >
-                <i className="fas fa-question" /> Sign up
-              </Link>
-            </li>
-            <li className="nav-item">
-              <span
-                to="/signup"
-                className="nav-link"
-                // style={isActive(history, "/signup")}
-                style={{ fontWeight: "800", cursor: "pointer" }}
-                onClick={() => signout(() => {
-                  history.push("/")
-                })}
-              >
-                Sign out
-              </span>
-            </li>
+            </li> */}
+            {!isAuthenticated() && (
+              <Fragment style={{ display: "inline-Block" }}>
+                <li className="nav-item">
+                  <Link
+                    to="/signin"
+                    className="nav-link"
+                    // style={isActive(history, "/signin")}
+                    style={{ fontWeight: "800" }}
+                  >
+                    <i className="fas fa-plus" /> Sign in
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="/signup"
+                    className="nav-link"
+                    // style={isActive(history, "/signup")}
+                    style={{ fontWeight: "800" }}
+                  >
+                    <i className="fas fa-question" /> Sign up
+                  </Link>
+                </li>
+              </Fragment>
+            )}
+            {isAuthenticated() && (
+              <li className="nav-item">
+                <span
+                  to="/signup"
+                  className="nav-link"
+                  // style={isActive(history, "/signup")}
+                  style={{ fontWeight: "800", cursor: "pointer" }}
+                  onClick={() =>
+                    signout(() => {
+                      history.push("/");
+                    })
+                  }
+                >
+                  Sign out
+                </span>
+              </li>
+            )}
           </ul>
         </div>
       </div>
