f5b65a534bbbb5e2b69cb310348ff075c98a623d
seun jay <seunjay92@gmail.com>
2019-08-06 01:45:28 +0100

    create methods to show user links, purchase history and info

--- src/components/layouts/UserDashboard.js
+++ src/components/layouts/UserDashboard.js
@@ -4,10 +4,53 @@ import { isAuthenticated } from "../../auth/index";
 import { signout } from "../../auth";
 import "./dashboard.css";
 
- function UserDashboard({history}) {
+function UserDashboard({ history }) {
   const {
     user: { firstName, email, role }
   } = isAuthenticated();
+
+  const userLinks = () => {
+    return (
+      <div className="card">
+        <h4 className="card-header">User Links</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link>My Cart</Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to="/profile/update">
+              Update Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const userInfo = () => {
+    return (
+      <div className="card mb-5">
+        <h3 className="card-header">User Information</h3>
+        <ul className="list-group">
+          <li className="list-group-item">{firstName}</li>
+          <li className="list-group-item">{email}</li>
+          <li className="list-group-item">{role === 1 ? "Admin" : "User"}</li>
+        </ul>
+      </div>
+    );
+  };
+
+  const purchaseHistory = () => {
+    return (
+      <div className="card mb-5">
+        <h3 className="card-header">Purchase</h3>
+        <ul className="list-group">
+          <li className="list-group-item">history</li>
+        </ul>
+      </div>
+    );
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-sm navbar-dark bg-warning mb-3 py-0">
@@ -56,32 +99,15 @@ import "./dashboard.css";
           </div>
         </div>
       </nav>
-      {/* <div className="sidebar">
-        <Link className="active" href="#home">
-          Home
-        </Link>
-        <Link href="#news">News</Link>
-        <Link href="#contact">Contact</Link>
-        <Link href="#about">About</Link>
-      </div> */}
-
-      <div className="card mb-5">
-        <h3 className="card-header">User Information</h3>
-        <ul className="list-group">
-          <li className="list-group-item">{firstName}</li>
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item">{role === 1 ? "Admin" : "User"}</li>
-        </ul>
-      </div>
-
-      <div className="card mb-5">
-        <h3 className="card-header">Purchase</h3>
-        <ul className="list-group">
-          <li className="list-group-item">history</li>
-        </ul>
+      <div className="row">
+        <div className="col-3">{userLinks()}</div>
+        <div className="col-9">
+          {userInfo()}
+          {purchaseHistory()}
+        </div>
       </div>
     </>
   );
 }
 
-export default withRouter(UserDashboard)
+export default withRouter(UserDashboard);
