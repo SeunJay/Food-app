2aab415e02d63e678d3cb6368a40b95744d27ff9
seun jay <seunjay92@gmail.com>
2019-08-06 01:54:55 +0100

    import AdminDashboard

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Home from "./components/pages/Home";
 //import NavBar from "./components/layouts/NavBar";
 import Signup from "./components/pages/Signup";
 import Signin from "./components/pages/Signin";
-import UserDashboard from "./components/layouts/UserDashboard"
-import PrivateRoute from "./auth/PrivateRoute"
+import UserDashboard from "./components/layouts/UserDashboard";
+import AdminDashboard from "./components/layouts/UserDashboard";
+import PrivateRoute from "./auth/PrivateRoute";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -19,6 +20,11 @@ function App() {
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/signin" component={Signin} />
           <PrivateRoute exact path="/userdashboard" component={UserDashboard} />
+          <PrivateRoute
+            exact
+            path="/admindashboard"
+            component={AdminDashboard}
+          />
         </Switch>
       </div>
     </Router>
