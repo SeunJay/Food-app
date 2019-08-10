7d5ba3ebf7eb21fdec0f189f52a6c1f084df5219
seun jay <seunjay92@gmail.com>
2019-08-10 05:48:30 +0100

    make lil change

--- src/components/layouts/ShowImage.js
+++ src/components/layouts/ShowImage.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 import { API } from "../../config";
 
-export default function ShowImage({item, url}) {
+export default function ShowImage({ item, url }) {
   return (
     <div className="food-img">
-      <img src={`${API}/${url}/photo/${item._id}`} alt={item.name} className="mb-3" style={{maxHeight: "100%", maxWidth: "100%"}}/>
+      <img
+        src={`${API}/${url}/photo/${item._id}`}
+        alt={item.name}
+        className="mb-3"
+        style={{ maxHeight: "100%", maxWidth: "100%" }}
+      />
     </div>
-  )
+  );
 }
