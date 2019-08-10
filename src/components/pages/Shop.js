e7bff895af776439ee9e6b5f63884abb47827d25
seun jay <seunjay92@gmail.com>
2019-08-10 23:23:13 +0100

    import Checkbox component

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "../layouts/NavBar";
 import { getCategories } from "../layouts/apiLayout";
+import Checkbox from "../layouts/Checkbox"
 
 export default function Shop() {
   const [categories, setCategories] = useState([]);
@@ -22,12 +23,11 @@ export default function Shop() {
     init()
   }, []);
 
-  console.log(categories)
   return (
     <>
       <NavBar brand="Omnifood" />
       <div className="row">
-        <div className="col-4">left sidebar</div>
+        <div className="col-4"><Checkbox category={categories}/></div>
         <div className="col-8">right sidebar</div>
       </div>
     </>
