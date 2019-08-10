679a2bff798a73d4e6549d204f707f6ef373f14b
seun jay <seunjay92@gmail.com>
2019-08-10 05:31:33 +0100

    create ShowImage component

---
+++ src/components/layouts/ShowImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { API } from "../../config";
+
+export default function ShowImage({item, url}) {
+  return (
+    <div className="food-img">
+      <img src={`${API}/${url}/photo/${item._id}`} alt={item.name} className="mb-3" style={{maxHeight: "100%", maxWidth: "100%"}}/>
+    </div>
+  )
+}
