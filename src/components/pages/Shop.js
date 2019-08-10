b3ffa78a2affa0483fbfc7936db98f2850d29280
seun jay <seunjay92@gmail.com>
2019-08-10 23:20:13 +0100

    import getCategory method with useState and useEffect

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -1,7 +1,28 @@
-import React from 'react';
-import NavBar from "../layouts/NavBar"
+import React, { useState, useEffect } from "react";
+import NavBar from "../layouts/NavBar";
+import { getCategories } from "../layouts/apiLayout";
 
 export default function Shop() {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+
+
+   const init = () => {
+     getCategories().then(data => {
+       if (data.error) {
+         setError(data.error);
+       } else {
+        setCategories(data)
+       }
+     });
+   };
+
+
+  useEffect(() => {
+    init()
+  }, []);
+
+  console.log(categories)
   return (
     <>
       <NavBar brand="Omnifood" />
