c89e1dd9acae03f18bb9e758711b0e69959b2584
seun jay <seunjay92@gmail.com>
2019-08-10 23:06:16 +0100

    create layout

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -5,6 +5,10 @@ export default function Shop() {
   return (
     <>
       <NavBar brand="Omnifood" />
+      <div className="row">
+        <div className="col-4">left sidebar</div>
+        <div className="col-8">right sidebar</div>
+      </div>
     </>
   );
 }
