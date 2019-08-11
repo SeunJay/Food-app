e8d5b1a6f1a924b4aee62c473c0174bfecc16803
seun jay <seunjay92@gmail.com>
2019-08-11 09:45:10 +0100

    import isAuthenticated method and Link

--- src/components/layouts/Profile.js
+++ src/components/layouts/Profile.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 import NavBar from "./NavBar";
+import { isAuthenticated } from "../../auth/index";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   return (
     <div>
-      <NavBar brand="Omnifood"/>
+      <NavBar brand="Omnifood" />
     </div>
-  )
+  );
 }
