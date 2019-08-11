3491479c0f2873e475fe8496a747b51a6f8db339
seun jay <seunjay92@gmail.com>
2019-08-11 10:50:55 +0100

    create form for updating profile

--- src/components/layouts/Profile.js
+++ src/components/layouts/Profile.js
@@ -25,8 +25,7 @@ export default function Profile({ match }) {
         setValues({
           ...values,
           firstName: data.firstName,
-          lastName: data.lastName,
-          password: data.password
+          lastName: data.lastName
         });
       }
     });
@@ -36,10 +35,65 @@ export default function Profile({ match }) {
     init(match.params.userId);
   }, []);
 
+  const handleChange = event => {};
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  const profileUpdate = (firstName, lastName, password) => {
+    return (
+      <div className="container col-md-8 offset-md-2">
+        <div className="card mb-3">
+          <div className="card-header text-center">Update profile</div>
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="" className="text-muted">
+                  Firt Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={handleChange("firstName")}
+                  value={values.firstName}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="" className="text-muted">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={handleChange("lastName")}
+                  value={values.lastName}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="" className="text-muted">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  onChange={handleChange("password")}
+                  value={values.password}
+                />
+              </div>
+              <button className="btn btn-warning">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
       <NavBar brand="Omnifood" />
-      {JSON.stringify(values)}
+      <h2 className="mb-4 text-center">Profile Update</h2>
+      {profileUpdate()}
     </div>
   );
 }
