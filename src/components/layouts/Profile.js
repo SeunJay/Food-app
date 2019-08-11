834d0a63d1837a31d649b89cf7da509a90bac858
seun jay <seunjay92@gmail.com>
2019-08-11 09:23:41 +0100

    import NavBar component

--- src/components/layouts/Profile.js
+++ src/components/layouts/Profile.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import NavBar from "./NavBar";
 
 export default function Profile() {
   return (
     <div>
-      Hi there
+      <NavBar brand="Omnifood"/>
     </div>
   )
 }
