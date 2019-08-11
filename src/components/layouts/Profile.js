6fca058528d79b4d9d0dd3785386986e16d7feaf
seun jay <seunjay92@gmail.com>
2019-08-11 10:08:39 +0100

    import useState and useEffect

--- src/components/layouts/Profile.js
+++ src/components/layouts/Profile.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import { isAuthenticated } from "../../auth/index";
 import { Link } from "react-router-dom";
+import { readUser, update, updateUser } from "../../user/apiuser";
 
-export default function Profile() {
+export default function Profile({ match }) {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    password: "",
+    error: false,
+    success: false
+  });
+
+  const { firstName, lastName, password, error, success } = values;
+
+  const { token } = isAuthenticated();
+
+  const init = userId => {
+    readUser(userId, token).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({
+          ...values,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          password: data.password
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init(match.params.userId);
+  }, []);
+
   return (
     <div>
       <NavBar brand="Omnifood" />
