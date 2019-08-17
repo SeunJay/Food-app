756d13adfab01e295218205db3425e86970b4ba1
seun jay <seunjay92@gmail.com>
2019-08-17 10:37:01 +0100

    create Search component

---
+++ src/components/layouts/Search.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { getCategories } from "./apiLayout";
+
+export default function Search() {
+  const [data, setData] = useState({
+    categories: [],
+    category: "",
+    search: "",
+    results: [],
+    searched: false
+  });
+
+  const { categories, category, search, results, searched } = data;
+
+  const loadCategories = () => {
+    getCategories().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setData({ ...data, categories: data });
+      }
+    });
+  };
+
+  console.log(categories);
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const searchData = () => {
+    console.log("search loading...")
+    console.log(category, search)
+  };
+
+  const searchSubmit = event => {
+    event.preventDefault();
+    searchData()
+  };
+
+  const handleChange = name => event => {
+    setData({ ...data, [name]: event.target.value, searched: false });
+  };
+
+  const searchForm = () => (
+    <form onSubmit={searchSubmit}>
+      <span className="input-group-text" style={{ background: "#f0f0f0" }}>
+        <div className="input-group warning input-group-lg">
+          <div className="input-group-prepend">
+            <select
+              className="btn mr-2"
+              onChange={handleChange("category")}
+              style={{ outline: "none" }}
+            >
+              <option value="All">Pick Category</option>
+              {categories.map((c, i) => (
+                <option key={i} value={c._id}>
+                  {c.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <input
+            type="search"
+            className="form-control"
+            onChange={handleChange("search")}
+            placeholder="Search by name"
+          />
+        </div>
+        <div className="btn input-group-append" style={{ border: "none" }}>
+          <button
+            className="input-group-text"
+            style={{ background: "#ffc107", color: "#fff", outline: "none" }}
+          >
+            Search
+          </button>
+        </div>
+      </span>
+    </form>
+  );
+
+  return (
+    <div className="row">
+      <div className="container">{searchForm()}</div>
+    </div>
+  );
+}
