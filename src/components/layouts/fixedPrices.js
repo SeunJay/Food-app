f314bb0b44f6359038e4950d19202f89f428c8c0
seun jay <seunjay92@gmail.com>
2019-08-17 02:38:15 +0100

    create prices for food

---
+++ src/components/layouts/fixedPrices.js
@@ -0,0 +1,37 @@
+export const prices = [
+         {
+           _id: 1,
+           name: "Any",
+           array: []
+         },
+         {
+           _id: 2,
+           name: "$50 to $99",
+           array: [50, 99]
+         },
+         {
+           _id: 3,
+           name: "$100 to $149",
+           array: [100, 149]
+         },
+         {
+           _id: 4,
+           name: "$150 to $199",
+           array: [150, 199]
+         },
+         {
+           _id: 5,
+           name: "$200 to $249",
+           array: [200, 249]
+         },
+         {
+           _id: 6,
+           name: "$250 to $299",
+           array: [250, 299]
+         },
+         {
+           _id: 6,
+           name: "More than $300",
+           array: [300, 500]
+         }
+       ];
