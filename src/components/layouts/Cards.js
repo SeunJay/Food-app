996ae5d6895e81a000639b72165c65a541dc9e0e
seun jay <seunjay92@gmail.com>
2019-08-17 04:38:38 +0100

    reduce food description characters

--- src/components/layouts/Cards.js
+++ src/components/layouts/Cards.js
@@ -9,7 +9,7 @@ export default function Cards({food}) {
         <div className="card-header">{food.name}</div>
         <div className="card-body">
           <ShowImage item={food} url="food"/>
-          <p>{food.description}</p>
+          <p>{food.description.substring(0,50)}</p>
           <p>${food.price}</p>
           <Link to="/">
             <button className="btn btn-outline-primary mt-2 mb-2 mr-2">
