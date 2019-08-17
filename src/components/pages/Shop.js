6f7c7deb84fb05d985b230eaf39bb8ad16199814
seun jay <seunjay92@gmail.com>
2019-08-17 01:51:20 +0100

    make change

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -1,27 +1,29 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "../layouts/NavBar";
 import { getCategories } from "../layouts/apiLayout";
-import Checkbox from "../layouts/Checkbox"
+import Checkbox from "../layouts/Checkbox";
 
 export default function Shop() {
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(false);
 
-
-   const init = () => {
-     getCategories().then(data => {
-       if (data.error) {
-         setError(data.error);
-       } else {
-        setCategories(data)
-       }
-     });
-   };
-
+  const init = () => {
+    getCategories().then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data);
+      }
+    });
+  };
 
   useEffect(() => {
-    init()
+    init();
   }, []);
+
+  const handleFilters = (filters, filterBy) => {
+    console.log(filters, filterBy);
+  };
 
   return (
     <>
@@ -30,7 +32,10 @@ export default function Shop() {
         <div className="row">
           <div className="col-4">
             <h4>Filter by Categories</h4>
-            <Checkbox categories={categories} />
+            <Checkbox
+              categories={categories}
+              handleFilters={filters => handleFilters(filters, "category")}
+            />
           </div>
           <div className="col-8">right sidebar</div>
         </div>
