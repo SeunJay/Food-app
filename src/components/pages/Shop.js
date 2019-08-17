0c5c24ec30c1482b986a93d46b099efbc0108067
seun jay <seunjay92@gmail.com>
2019-08-17 03:43:01 +0100

    import RadioBox component

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -3,6 +3,7 @@ import NavBar from "../layouts/NavBar";
 import { getCategories } from "../layouts/apiLayout";
 import Checkbox from "../layouts/Checkbox";
 import { prices } from "../layouts/fixedPrices";
+import RadioBox from "../layouts/RadioBox";
 
 export default function Shop() {
   const [myFilters, setMyFilters] = useState({
@@ -28,20 +29,47 @@ export default function Shop() {
   const handleFilters = (filters, filterBy) => {
     const newFilters = { ...myFilters };
     newFilters.filters[filterBy] = filters;
+
+    if (filterBy === "price") {
+      let priceValues = handlePrice(filters);
+      newFilters.filters[filterBy] = priceValues;
+    }
+
     setMyFilters(newFilters);
+  };
+
+  const handlePrice = id => {
+    const data = prices;
+    let array = []
+
+    for(let key in data){
+      if(data[key]._id === parseInt(id)){
+        array = data[key].array
+      }
+    }
+    return array
   };
 
   return (
     <>
       <NavBar brand="Omnifood" />
-      <div className="container-fluid offset-md-1">
+      <div className="">
         <div className="row">
           <div className="col-4">
             <h4>Filter by Categories</h4>
-            <Checkbox
-              categories={categories}
-              handleFilters={filters => handleFilters(filters, "category")}
-            />
+            <ul>
+              <Checkbox
+                categories={categories}
+                handleFilters={filters => handleFilters(filters, "category")}
+              />
+            </ul>
+            <h4>Filter by Price range</h4>
+            <div>
+              <RadioBox
+                prices={prices}
+                handleFilters={filters => handleFilters(filters, "price")}
+              />
+            </div>
           </div>
           <div className="col-8">{JSON.stringify(myFilters)}</div>
         </div>
