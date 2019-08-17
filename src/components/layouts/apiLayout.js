f6256d30ff36fe2cdbad24b4a8af6bf40abba7fd
seun jay <seunjay92@gmail.com>
2019-08-17 04:17:38 +0100

    add getFilteredFoods method

--- src/components/layouts/apiLayout.js
+++ src/components/layouts/apiLayout.js
@@ -1,6 +1,6 @@
 import { API } from "../../config";
 
-export const getFoods = (sortBy) => {
+export const getFoods = sortBy => {
   return fetch(`${API}/foods?sortBy=${sortBy}&order=desc&limit=6`, {
     method: "GET"
   })
@@ -10,10 +10,29 @@ export const getFoods = (sortBy) => {
     .catch(err => console.log(err));
 };
 
-
 export const getCategories = () => {
   return fetch(`${API}/categories`, {
     method: "GET"
+  })
+    .then(res => {
+      return res.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const getFilteredFoods = (skip, limit, filters = {}) => {
+  const data = {
+    skip,
+    limit,
+    filters
+  };
+  return fetch(`${API}/foods/by/search`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
   })
     .then(res => {
       return res.json();
