ea98384d78d461c61e7124a3b7d78507881f4b13
seun jay <seunjay92@gmail.com>
2019-08-18 20:15:12 +0100

    change styles

--- src/components/layouts/Cards.js
+++ src/components/layouts/Cards.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import {Link} from "react-router-dom";
-import ShowImage from "./ShowImage"
+import React from "react";
+import { Link } from "react-router-dom";
+import ShowImage from "./ShowImage";
 
-export default function Cards({food}) {
+export default function Cards({ food, showViewFoodButton = true }) {
+
+  const showViewButton = (showViewFoodButton)=>{
+    return (
+      showViewFoodButton && (
+        <button className="btn btn-outline-primary mt-2 mb-2">View Food</button>
+      )
+    )
+  }
   return (
-    <div className="col-4 mb-3">
-      <div className="card">
-        <div className="card-header">{food.name}</div>
-        <div className="card-body">
-          <ShowImage item={food} url="food"/>
-          <p>{food.description.substring(0,50)}</p>
-          <p>${food.price}</p>
-          <Link to="/">
-            <button className="btn btn-outline-primary mt-2 mb-2 mr-2">
-              View Food
-            </button>
-            <button className="btn btn-outline-warning mt-2 mb-2">
-              Add to cart
-            </button>
-          </Link>
-        </div>
+    <div className="card">
+      <div className="card-header">{food.name}</div>
+      <div className="card-body">
+        <ShowImage item={food} url="food" />
+        <p>{food.description.substring(0, 50)}</p>
+        <p>${food.price}</p>
+        <Link to={`/food/${food._id}`}>
+         {showViewButton(showViewFoodButton)}
+        </Link>
+        <button className="btn btn-outline-warning mt-2 mb-2">
+          Add to cart
+        </button>
       </div>
     </div>
   );
