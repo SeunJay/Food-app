1eb17515d09da0a84bd40f6dc342de2324000f29
seun jay <seunjay92@gmail.com>
2019-08-18 19:10:08 +0100

    import readFood method

--- src/components/layouts/Food.js
+++ src/components/layouts/Food.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NavBar from "./NavBar";
+import {readFood} from "../layouts/apiLayout"
 
-export default function Food() {
+export default function Food({match}) {
+  const [food, setFood] = useState({});
+  const [error, setError] = useState(false);
+
+  const loadSingleFood = foodId =>{
+    readFood(foodId).then(data => {
+      if(data.error){
+        setError(data.error)
+      } else {
+        setFood(data)
+      }
+    })
+  }
+
+  useEffect(()=>{
+    const foodId = match.params.foodId;
+    loadSingleFood(foodId)
+  }, [])
+
+  console.log(food)
+
   return (
     <div>
       <NavBar brand="Omnifood"/>
-      <h1 className="text-center">Food Page</h1>
+      <h1 className="mb-4 text-center">Food Page</h1>
     </div>
   );
 }
