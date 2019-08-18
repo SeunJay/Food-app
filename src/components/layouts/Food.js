f77da08ff1a77b34aba9985ce05758d6cd324809
seun jay <seunjay92@gmail.com>
2019-08-18 18:47:19 +0100

    create Food component

---
+++ src/components/layouts/Food.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+export default function Food() {
+  return (
+    <div>
+      <NavBar brand="Omnifood"/>
+      <h1 className="text-center">Food Page</h1>
+    </div>
+  );
+}
