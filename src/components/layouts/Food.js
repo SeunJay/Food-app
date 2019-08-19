2cd9c9d5b54943fd88d03f7db5525cfa8194a68b
seun jay <seunjay92@gmail.com>
2019-08-19 07:44:32 +0100

    import "listRelated" method

--- src/components/layouts/Food.js
+++ src/components/layouts/Food.js
@@ -1,36 +1,57 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
-import {readFood} from "../layouts/apiLayout"
-import Cards from "./Cards"
+import { readFood, listRelated } from "../layouts/apiLayout";
+import Cards from "./Cards";
 
-export default function Food({match}) {
+export default function Food({ match }) {
   const [food, setFood] = useState({});
   const [error, setError] = useState(false);
+  const [relatedFood, setRelatedFood] = useState([])
 
-  const loadSingleFood = foodId =>{
+  const loadSingleFood = foodId => {
     readFood(foodId).then(data => {
-      if(data.error){
-        setError(data.error)
+      if (data.error) {
+        setError(data.error);
       } else {
-        setFood(data)
+        setFood(data);
+        //fetch related foods
+        listRelated(data._id).then(data => {
+          if(data.error){
+            setError(data.error)
+          } else {
+            setRelatedFood(data)
+          }
+        })
       }
-    })
-  }
+    });
+  };
 
-  useEffect(()=>{
+  console.log(relatedFood)
+
+  useEffect(() => {
     const foodId = match.params.foodId;
-    loadSingleFood(foodId)
-  }, [])
+    loadSingleFood(foodId);
+  }, [match]);
 
-  console.log(food)
+  console.log(food);
 
   return (
     <>
       <NavBar brand="Omnifood" />
-      <h1 className="mb-4 text-center">Food Page</h1>
+      <h1 className="mb-4 text-center">{food.name} Page</h1>
       <div className="row">
-        <div className="col-5 mb-4" style={{margin: "0 auto"}}>
-          {food && food.description && <Cards food={food} showViewFoodButton={false}/>}
+        <div className="col-8 mb-4">
+          {food && food.description && (
+            <Cards food={food} showViewFoodButton={false} />
+          )}
+        </div>
+        <div className="col-4">
+          <h4>Related Food</h4>
+          {relatedFood.map((f, i)=>(
+            <div className="mb-3">
+              <Cards key={i} food={f}/>
+            </div>
+          ))}
         </div>
       </div>
     </>
